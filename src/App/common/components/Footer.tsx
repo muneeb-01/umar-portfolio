95ba38da90382d01edabcae7a0e20a8b39e5f146
muneeb-01 <muneerahmed8556@gmail.com>
2026-01-07 19:32:10 +0500

    Removing Unused components

--- src/App/common/components/Footer.tsx
+++ src/App/common/components/Footer.tsx
@@ -1,7 +1,6 @@
 import { useRef, useEffect } from "react"
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { Button } from "./Button"
 
 gsap.registerPlugin(ScrollTrigger)
 
